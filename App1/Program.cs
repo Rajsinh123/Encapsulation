154f144bd14463b6f3ce67023dfafef3674f4463
Rajsinh123 <rajsinhmandawale15@gmail.com>
2024-06-03 16:22:09 +0530

    Initial Commit

---
+++ App1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Encapsulation
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encapsulation eb = new Encapsulation();
+            eb.Id = 11;
+            eb.Name = "RAMA";
+            eb.Email = "Rama@gmail.com";
+            Console.WriteLine("Id :" + eb.Id);
+            Console.WriteLine("Name :" + eb.Name);
+            Console.WriteLine("Email :" + eb.Email);
+            Console.ReadLine();
+        }
+
+    }
+}
